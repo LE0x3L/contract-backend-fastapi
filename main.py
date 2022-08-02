1a860b4100d918cc3d363957b5913411e51cf3b1
LeoUrrego <trecetp@gmail.com>
2022-08-02 14:04:27 -0500

    Validate path parameter

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 
 #FastAPI
 from fastapi import FastAPI
-
+from fastapi import Path
 
 http_provider = os.environ['URL_HTTP_PROVIDER']
 w3 = web3.Web3( web3.HTTPProvider( http_provider ) )
@@ -22,8 +22,17 @@ async def root():
     return {"message": "Contrat fastAPI"}
 
 # Return the house name
-@app.get("/HOUSE_NAME/{house_addr}")
-async def read_house_name(house_addr: str):
+@app.get("/{house_addr}/HOUSE_NAME")
+async def read_house_name(
+    house_addr: str = Path(
+        ...,
+        min_length=42,
+        max_length=42,
+        title="CLHouse Address",
+        description="CLHouse to get the name",
+        example="0xD6585C7CCdAB149DDE245bB9CC2418EBB7750427"
+    )
+):
     CLH = w3.eth.contract(address=house_addr, abi=abiCLH)
     house_name = CLH.functions.HOUSE_NAME().call()
     return {"house_name": house_name}
